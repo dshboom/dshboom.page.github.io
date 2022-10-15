9e075ce751b0619066783f4d449e4671be014193
dshboom <110819392+dshboom@users.noreply.github.com>
2022-10-15 18:23:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function go(zone,gotype)
 {
   if(gotype == "page")
   {
-    var a = "./tree/main/";
+    var a = "tree/main/";
     var res = a.concat(zone);
     window.open(res, "_search");
   }
   else if(gotype == "index")
   {
-    var a = ".tree/main/page/";
+    var a = "tree/main/page/";
     var res = a.concat(zone);
     window.open(res, "_search");
   }
